e7253d685029cddaf913795e6f8f5d81cc2c53a6
Bobby House <bobbyh@aweber.com>
2012-10-30 20:41:46 -0400

    Parse IPs

--- parse.py
+++ parse.py
@@ -6,44 +6,24 @@ import urllib2
 from datetime import datetime
 
 
-# Globals
-# -------
-PATH = './'
-FILE = 'tor-ips'
-FILETYPE = 'csv'
 URI = 'https://www.dan.me.uk/torlist'
+
+message= "CEF:0|Tor Node|Tor Nodes 0.1|100|1|Malicious {0}|1|shost={0} cs1={0} \
+cs1Label=Source  cs2={0} cs2Label=ConfidenceLevel cs3={0} \
+cs3Label=Description"
+
+ip_regex = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
 
 
 def main():
 
-    # Get the current date time and format
-    # i.e. Month-Day-YearTHour:Minute:Second
-    timestamp = datetime.now().strftime("%m-%d-%yT%H:%M:%S")
-
-    # Use the constants defined earlier to create our filename
-    file_name = "{0}-{1}.{2}".format(FILE, timestamp, FILETYPE)
-
-    # Use the constants defined earlier to get the absolute path of the file
-    # that we will write to
-    path_to_file = os.path.join(os.path.abspath(PATH), file_name)
-
-
     try:
         # Get the data from the server
         resp = urllib2.urlopen(URI).read()
-
-        # Open a file for writing
-        # If the file already exists it will be overwritten
-        f = open(path_to_file, 'w')
-
-        # Write the header of the file
-        f.write("Addy\n")
-
-        # Write the data to the file
-        f.write(resp)
-
-        # Close stream to open file
-        f.close()
+        ips = resp.split('\n')
+        for ip in ips:
+            if ip_regex.match(ip):
+                print message.format(ip)
 
     except urllib2.HTTPError, e:
         print e
@@ -51,7 +31,6 @@ def main():
 
 
     sys.exit(0)
-
 
 
 if __name__ == "__main__":
