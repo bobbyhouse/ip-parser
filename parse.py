dec49fdb249ea1ce80203bc55f043036e7c3ffe1
Antonio Sena <sena.antoniojr@gmail.com>
2012-11-01 22:40:40 -0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
-
+# Tor node Parser
+# Scrapes https://www.dan.me.uk/torlist for ip list import into SIEM via CEF/Syslog
+# bobbyhouse at gmail.com
+# https://github.com/bobbyhouse/ip-parser
+#
+#----------------------------------------------------------------------------
+# 
+#Copyright 2012 Bobby House
+#
 import os
 import sys
 import urllib2
