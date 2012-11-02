3e9d137bf707e435e6d414b5c138980acbbae5f4
Antonio Sena <sena.antoniojr@gmail.com>
2012-11-01 22:42:31 -0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -17,7 +17,7 @@ from datetime import datetime
 
 URI = 'https://www.dan.me.uk/torlist'
 
-message= "CEF:0|Tor Node|Tor Node |307|1|Suspect Tor Node {0}|7|src={0} "
+message= "CEF:0|Tor Node|Tor Node |0.0.7|1|Suspect Tor Node |7|src={0} "
 
 ip_regex = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
 
